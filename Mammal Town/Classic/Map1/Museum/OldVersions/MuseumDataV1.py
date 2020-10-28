1168d352a66749fe676107095233c0247c49049a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-28 15:17:55 -0700

    MuseumDataV1.py
    An archive of the first version of the museum data file.

---
+++ Mammal Town/Classic/Map1/Museum/OldVersions/MuseumDataV1.py
@@ -0,0 +1,425 @@
+# Start of script
+# Mammal town - Classic (Map 1 >> Museum)
+print ("Museum collection data")
+"""
+0 = Empty
+1 = Full
+2/else = A bug that needs to be fixed
+"""
+print ("This is a Python data file, and is not meant to be opened while the game is playing")
+""" Divider """
+museumRoom1():
+  # Fossils
+  # Fossil size 3
+  fossilDisplay1AStatus = 0; # T-Rex Tail
+  fossilDisplay1BStatus = 0; # T-Rex Torso
+  fossilDisplay1CStatus = 0; # T-Rex Skull
+  fossilDisplay2AStatus = 0; # Stego tail
+  fossilDisplay2BStatus = 0; # Stego torso
+  fossilDisplay2CStatus = 0; # Stego skull
+  fossilDisplay3AStatus = 0; # Add new dinosaur
+  fossilDisplay3BStatus = 0;
+  fossilDisplay3CStatus = 0;
+  fossilDisplay4AStatus = 0; # Add new dinosaur
+  fossilDisplay4BStatus = 0;
+  fossilDisplay4CStatus = 0;
+  fossilDisplay5AStatus = 0; # Add new dinosaur
+  fossilDisplay5BStatus = 0;
+  fossilDisplay5CStatus = 0;
+  fossilDisplay6AStatus = 0; # Add new dinosaur
+  fossilDisplay6BStatus = 0;
+  fossilDisplay6CStatus = 0;
+  fossilDisplay7AStatus = 0; # Add new dinosaur
+  fossilDisplay7BStatus = 0;
+  fossilDisplay7CStatus = 0;
+  fossilDisplay8AStatus = 0; # Add new dinosaur
+  fossilDisplay8BStatus = 0;
+  fossilDisplay8CStatus = 0;
+  fossilDisplay9AStatus = 0; # Add new dinosaur
+  fossilDisplay9BStatus = 0;
+  fossilDisplay9CStatus = 0;
+  fossilDisplay10AStatus = 0; # Add new dinosaur
+  fossilDisplay10BStatus = 0;
+  fossilDisplay10CStatus = 0;
+  fossilDisplay11AStatus = 0; # Add new dinosaur
+  fossilDisplay11BStatus = 0;
+  fossilDisplay11CStatus = 0;
+  fossilDisplay12AStatus = 0; # Add new dinosaur
+  fossilDisplay12BStatus = 0;
+  fossilDisplay12CStatus = 0;
+  fossilDisplay13AStatus = 0; # Add new dinosaur
+  fossilDisplay13BStatus = 0;
+  fossilDisplay13CStatus = 0;
+  fossilDisplay14AStatus = 0; # Add new dinosaur
+  fossilDisplay14BStatus = 0;
+  fossilDisplay14CStatus = 0;
+  fossilDisplay15AStatus = 0; # Add new dinosaur
+  fossilDisplay15BStatus = 0;
+  fossilDisplay15CStatus = 0;
+  fossilDisplay16AStatus = 0; # Add new dinosaur
+  fossilDisplay16BStatus = 0;
+  fossilDisplay16CStatus = 0;
+  fossilDisplay17AStatus = 0; # Add new dinosaur
+  fossilDisplay17BStatus = 0;
+  fossilDisplay17CStatus = 0;
+  fossilDisplay18AStatus = 0; # Add new dinosaur
+  fossilDisplay18BStatus = 0;
+  fossilDisplay18CStatus = 0;
+  fossilDisplay19AStatus = 0; # Add new dinosaur
+  fossilDisplay19BStatus = 0;
+  fossilDisplay19CStatus = 0;
+  fossilDisplay20AStatus = 0; # Add new dinosaur
+  fossilDisplay20BStatus = 0;
+  fossilDisplay20CStatus = 0;
+  # Fossil size 2
+  
+  # Fossil size 1
+""" Divider """
+museumRoom2():
+  # Fish
+  # Fish size 3
+  fishDisplay1AStatus = 0; # Great white shark tail 
+  fishDisplay1BStatus = 0; # Great white shark torso
+  fishDisplay1CStatus = 0; # Great white shark skull
+  fishDisplay2AStatus = 0; # Add new large fish
+  fishDisplay2BStatus = 0; # 
+  fishDisplay2CStatus = 0; # 
+  fishDisplay3AStatus = 0; # Add new large fish
+  fishDisplay3BStatus = 0;
+  fishDisplay3CStatus = 0;
+  fishDisplay4AStatus = 0; # Add new large fish
+  fishDisplay4BStatus = 0;
+  fishDisplay4CStatus = 0;
+  fishDisplay5AStatus = 0; # Add new large fish
+  fishDisplay5BStatus = 0;
+  fishDisplay5CStatus = 0;
+  fishDisplay6AStatus = 0; # Add new large fish
+  fishDisplay6BStatus = 0;
+  fishDisplay6CStatus = 0;
+  fishDisplay7AStatus = 0; # Add new large fish
+  fishDisplay7BStatus = 0;
+  fishDisplay7CStatus = 0;
+  fishDisplay8AStatus = 0; # Add new large fish
+  fishDisplay8BStatus = 0;
+  fishDisplay8CStatus = 0;
+  fishDisplay9AStatus = 0; # Add new large fish
+  fishDisplay9BStatus = 0;
+  fishDisplay9CStatus = 0;
+  fishDisplay10AStatus = 0; # Add new large fish
+  fishDisplay10BStatus = 0;
+  fishDisplay10CStatus = 0;
+  fishDisplay11AStatus = 0; # Add new large fish
+  fishDisplay11BStatus = 0;
+  fishDisplay11CStatus = 0;
+  fishDisplay12AStatus = 0; # Add new large fish
+  fishDisplay12BStatus = 0;
+  fishDisplay12CStatus = 0;
+  fishDisplay13AStatus = 0; # Add new large fish
+  fishDisplay13BStatus = 0;
+  fishDisplay13CStatus = 0;
+  fishDisplay14AStatus = 0; # Add new large fish
+  fishDisplay14BStatus = 0;
+  fishDisplay14CStatus = 0;
+  fishDisplay15AStatus = 0; # Add new large fish
+  fishDisplay15BStatus = 0;
+  fishDisplay15CStatus = 0;
+  fishDisplay16AStatus = 0; # Add new large fish
+  fishDisplay16BStatus = 0;
+  fishDisplay16CStatus = 0;
+  fishDisplay17AStatus = 0; # Add new large fish
+  fishDisplay17BStatus = 0;
+  fishDisplay17CStatus = 0;
+  fishDisplay18AStatus = 0; # Add new large fish
+  fishDisplay18BStatus = 0;
+  fishDisplay18CStatus = 0;
+  fishDisplay19AStatus = 0; # Add new large fish
+  fishDisplay19BStatus = 0;
+  fishDisplay19CStatus = 0;
+  fishDisplay20AStatus = 0; # Add new large fish
+  fishDisplay20BStatus = 0;
+  fishDisplay20CStatus = 0;
+  # Fish size 2
+  
+  # Fish size 1
+""" Divider """
+museumRoom3():
+  # Insects
+  # Giant insects
+  insectDisplay1AStatus = 0; # Giant tarantula left legs
+  insectDisplay1BStatus = 0; # Giant tarantula torso and head
+  insectDisplay1CStatus = 0; # Giant tarantula right legs
+  insectDisplay2AStatus = 0; # Add new large insect
+  insectDisplay2BStatus = 0; # 
+  insectDisplay2CStatus = 0; # 
+  insectDisplay3AStatus = 0; # Add new large insect
+  insectDisplay3BStatus = 0;
+  insectDisplay3CStatus = 0;
+  insectDisplay4AStatus = 0; # Add new large insect
+  insectDisplay4BStatus = 0;
+  insectDisplay4CStatus = 0;
+  insectDisplay5AStatus = 0; # Add new large insect
+  insectDisplay5BStatus = 0;
+  insectDisplay5CStatus = 0;
+  insectDisplay6AStatus = 0; # Add new large insect
+  insectDisplay6BStatus = 0;
+  insectDisplay6CStatus = 0;
+  insectDisplay7AStatus = 0; # Add new large insect
+  insectDisplay7BStatus = 0;
+  insectDisplay7CStatus = 0;
+  insectDisplay8AStatus = 0; # Add new large insect
+  insectDisplay8BStatus = 0;
+  insectDisplay8CStatus = 0;
+  insectDisplay9AStatus = 0; # Add new large insect
+  insectDisplay9BStatus = 0;
+  insectDisplay9CStatus = 0;
+  insectDisplay10AStatus = 0; # Add new large insect
+  insectDisplay10BStatus = 0;
+  insectDisplay10CStatus = 0;
+  insectDisplay11AStatus = 0; # Add new large insect
+  insectDisplay11BStatus = 0;
+  insectDisplay11CStatus = 0;
+  insectDisplay12AStatus = 0; # Add new large insect
+  insectDisplay12BStatus = 0;
+  insectDisplay12CStatus = 0;
+  insectDisplay13AStatus = 0; # Add new large insect
+  insectDisplay13BStatus = 0;
+  insectDisplay13CStatus = 0;
+  insectDisplay14AStatus = 0; # Add new large insect
+  insectDisplay14BStatus = 0;
+  insectDisplay14CStatus = 0;
+  insectDisplay15AStatus = 0; # Add new large insect
+  insectDisplay15BStatus = 0;
+  insectDisplay15CStatus = 0;
+  insectDisplay16AStatus = 0; # Add new large insect
+  insectDisplay16BStatus = 0;
+  insectDisplay16CStatus = 0;
+  insectDisplay17AStatus = 0; # Add new large insect
+  insectDisplay17BStatus = 0;
+  insectDisplay17CStatus = 0;
+  insectDisplay18AStatus = 0; # Add new large insect
+  insectDisplay18BStatus = 0;
+  insectDisplay18CStatus = 0;
+  insectDisplay19AStatus = 0; # Add new large insect
+  insectDisplay19BStatus = 0;
+  insectDisplay19CStatus = 0;
+  insectDisplay20AStatus = 0; # Add new large insect
+  insectDisplay20BStatus = 0;
+  insectDisplay20CStatus = 0;
+  # Bug size 2
+  
+  # Bug size 1
+""" Divider """
+museumRoom4():
+  # Bugs
+  # Giant bugs
+  bugDisplay1AStatus = 0; # Giant tarantula left legs
+  bugDisplay1BStatus = 0; # Giant tarantula torso and head
+  bugDisplay1CStatus = 0; # Giant tarantula right legs
+  bugDisplay2AStatus = 0; # Add new large bug
+  bugDisplay2BStatus = 0; # 
+  bugDisplay2CStatus = 0; # 
+  bugDisplay3AStatus = 0; # Add new large bug
+  bugDisplay3BStatus = 0;
+  bugDisplay3CStatus = 0;
+  bugDisplay4AStatus = 0; # Add new large bug
+  bugDisplay4BStatus = 0;
+  bugDisplay4CStatus = 0;
+  bugDisplay5AStatus = 0; # Add new large bug
+  bugDisplay5BStatus = 0;
+  bugDisplay5CStatus = 0;
+  bugDisplay6AStatus = 0; # Add new large bug
+  bugDisplay6BStatus = 0;
+  bugDisplay6CStatus = 0;
+  bugDisplay7AStatus = 0; # Add new large bug
+  bugDisplay7BStatus = 0;
+  bugDisplay7CStatus = 0;
+  bugDisplay8AStatus = 0; # Add new large bug
+  bugDisplay8BStatus = 0;
+  bugDisplay8CStatus = 0;
+  bugDisplay9AStatus = 0; # Add new large bug
+  bugDisplay9BStatus = 0;
+  bugDisplay9CStatus = 0;
+  bugDisplay10AStatus = 0; # Add new large bug
+  bugDisplay10BStatus = 0;
+  bugDisplay10CStatus = 0;
+  bugDisplay11AStatus = 0; # Add new large bug
+  bugDisplay11BStatus = 0;
+  bugDisplay11CStatus = 0;
+  bugDisplay12AStatus = 0; # Add new large bug
+  bugDisplay12BStatus = 0;
+  bugDisplay12CStatus = 0;
+  bugDisplay13AStatus = 0; # Add new large bug
+  bugDisplay13BStatus = 0;
+  bugDisplay13CStatus = 0;
+  bugDisplay14AStatus = 0; # Add new large bug
+  bugDisplay14BStatus = 0;
+  bugDisplay14CStatus = 0;
+  bugDisplay15AStatus = 0; # Add new large bug
+  bugDisplay15BStatus = 0;
+  bugDisplay15CStatus = 0;
+  bugDisplay16AStatus = 0; # Add new large bug
+  bugDisplay16BStatus = 0;
+  bugDisplay16CStatus = 0;
+  bugDisplay17AStatus = 0; # Add new large bug
+  bugDisplay17BStatus = 0;
+  bugDisplay17CStatus = 0;
+  bugDisplay18AStatus = 0; # Add new large bug
+  bugDisplay18BStatus = 0;
+  bugDisplay18CStatus = 0;
+  bugDisplay19AStatus = 0; # Add new large bug
+  bugDisplay19BStatus = 0;
+  bugDisplay19CStatus = 0;
+  bugDisplay20AStatus = 0; # Add new large bug
+  bugDisplay20BStatus = 0;
+  bugDisplay20CStatus = 0;
+  # Bug size 2
+  
+  # Bug size 1
+""" Divider """
+museumRoom5(): # New
+  # Artwork
+  # Giant art
+  artDisplay1AStatus = 0; # Giant Mona Lisa canvas part 1
+  artDisplay1BStatus = 0; # Giant Mona Lisa canvas part 2
+  artDisplay1CStatus = 0; # Giant Mona Lisa canvas part 3
+  artDisplay2AStatus = 0; # Add new large art
+  artDisplay2BStatus = 0; # 
+  artDisplay2CStatus = 0; # 
+  artDisplay3AStatus = 0; # Add new large art
+  artDisplay3BStatus = 0;
+  artDisplay3CStatus = 0;
+  artDisplay4AStatus = 0; # Add new large art
+  artDisplay4BStatus = 0;
+  artDisplay4CStatus = 0;
+  artDisplay5AStatus = 0; # Add new large art
+  artDisplay5BStatus = 0;
+  artDisplay5CStatus = 0;
+  artDisplay6AStatus = 0; # Add new large art
+  artDisplay6BStatus = 0;
+  artDisplay6CStatus = 0;
+  artDisplay7AStatus = 0; # Add new large art
+  artDisplay7BStatus = 0;
+  artDisplay7CStatus = 0;
+  artDisplay8AStatus = 0; # Add new large art
+  artDisplay8BStatus = 0;
+  artDisplay8CStatus = 0;
+  artDisplay9AStatus = 0; # Add new large art
+  artDisplay9BStatus = 0;
+  artDisplay9CStatus = 0;
+  artDisplay10AStatus = 0; # Add new large art
+  artDisplay10BStatus = 0;
+  artDisplay10CStatus = 0;
+  artDisplay11AStatus = 0; # Add new large art
+  artDisplay11BStatus = 0;
+  artDisplay11CStatus = 0;
+  artDisplay12AStatus = 0; # Add new large art
+  artDisplay12BStatus = 0;
+  artDisplay12CStatus = 0;
+  artDisplay13AStatus = 0; # Add new large art
+  artDisplay13BStatus = 0;
+  artDisplay13CStatus = 0;
+  artDisplay14AStatus = 0; # Add new large art
+  artDisplay14BStatus = 0;
+  artDisplay14CStatus = 0;
+  artDisplay15AStatus = 0; # Add new large art
+  artDisplay15BStatus = 0;
+  artDisplay15CStatus = 0;
+  artDisplay16AStatus = 0; # Add new large art
+  artDisplay16BStatus = 0;
+  artDisplay16CStatus = 0;
+  artDisplay17AStatus = 0; # Add new large art
+  artDisplay17BStatus = 0;
+  artDisplay17CStatus = 0;
+  artDisplay18AStatus = 0; # Add new large art
+  artDisplay18BStatus = 0;
+  artDisplay18CStatus = 0;
+  artDisplay19AStatus = 0; # Add new large art
+  artDisplay19BStatus = 0;
+  artDisplay19CStatus = 0;
+  artDisplay20AStatus = 0; # Add new large art
+  artDisplay20BStatus = 0;
+  artDisplay20CStatus = 0;
+  # Art size 2
+  
+  # Art size 1
+  artDisplayC1Status=0; # Bliss (wallpaper)
+""" Divider """
+museumRoom6(): # New
+  # Technology
+  # Giant tech
+  techDisplay1AStatus = 0; # ENIAC engine
+  techDisplay1BStatus = 0; # ENIAC mainframe
+  techDisplay1CStatus = 0; # ENIAC Controls
+  techDisplay2AStatus = 0; # Add new large tech
+  techDisplay2BStatus = 0; # 
+  techDisplay2CStatus = 0; # 
+  techDisplay3AStatus = 0; # Add new large tech
+  techDisplay3BStatus = 0;
+  techDisplay3CStatus = 0;
+  techDisplay4AStatus = 0; # Add new large tech
+  techDisplay4BStatus = 0;
+  techDisplay4CStatus = 0;
+  techDisplay5AStatus = 0; # Add new large tech
+  techDisplay5BStatus = 0;
+  techDisplay5CStatus = 0;
+  techDisplay6AStatus = 0; # Add new large tech
+  techDisplay6BStatus = 0;
+  techDisplay6CStatus = 0;
+  techDisplay7AStatus = 0; # Add new large tech
+  techDisplay7BStatus = 0;
+  techDisplay7CStatus = 0;
+  techDisplay8AStatus = 0; # Add new large tech
+  techDisplay8BStatus = 0;
+  techDisplay8CStatus = 0;
+  techDisplay9AStatus = 0; # Add new large tech
+  techDisplay9BStatus = 0;
+  techDisplay9CStatus = 0;
+  techDisplay10AStatus = 0; # Add new large tech
+  techDisplay10BStatus = 0;
+  techDisplay10CStatus = 0;
+  techDisplay11AStatus = 0; # Add new large tech
+  techDisplay11BStatus = 0;
+  techDisplay11CStatus = 0;
+  techDisplay12AStatus = 0; # Add new large tech
+  techDisplay12BStatus = 0;
+  techDisplay12CStatus = 0;
+  techDisplay13AStatus = 0; # Add new large tech
+  techDisplay13BStatus = 0;
+  techDisplay13CStatus = 0;
+  techDisplay14AStatus = 0; # Add new large tech
+  techDisplay14BStatus = 0;
+  techDisplay14CStatus = 0;
+  techDisplay15AStatus = 0; # Add new large tech
+  techDisplay15BStatus = 0;
+  techDisplay15CStatus = 0;
+  techDisplay16AStatus = 0; # Add new large tech
+  techDisplay16BStatus = 0;
+  techDisplay16CStatus = 0;
+  techDisplay17AStatus = 0; # Add new large tech
+  techDisplay17BStatus = 0;
+  techDisplay17CStatus = 0;
+  techDisplay18AStatus = 0; # Add new large tech
+  techDisplay18BStatus = 0;
+  techDisplay18CStatus = 0;
+  techDisplay19AStatus = 0; # Add new large tech
+  techDisplay19BStatus = 0;
+  techDisplay19CStatus = 0;
+  techDisplay20AStatus = 0; # Add new large tech
+  techDisplay20BStatus = 0;
+  techDisplay20CStatus = 0;
+  # Tech size 2
+  
+  # Tech size 1
+  
+""" Divider """
+
+# Museum curaters
+
+''' None listed '''
+
+""" File info
+File version: 1 (Wednesday, October 28th 2020 at 3:17 pm)
+File type: Python script (*.py)
+Line count (including blank lines and compiler line): 426
+"""
+# End of script
