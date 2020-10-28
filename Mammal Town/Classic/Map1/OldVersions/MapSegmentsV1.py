f7fd112432437e7c1265ef8c128a55c0faec56b3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-28 15:24:31 -0700

    MapSegmentsV1.py
    An archive of the first version of the map segments file.

---
+++ Mammal Town/Classic/Map1/OldVersions/MapSegmentsV1.py
@@ -0,0 +1,87 @@
+# Start of script
+# Map segments
+
+# Small village (classic) - 25 acres
+
+'''
+A1-A5
+B1-B5
+C1-C5
+D1-D5
+E1-E5
+'''
+
+# Medium village - 81 acres
+
+'''
+A1-A9
+B1-B9
+C1-C9
+D1-D9
+E1-E9
+F1-F9
+G1-G9
+H1-H9
+I1-I9
+'''
+
+# Large village - 225 acres
+
+'''
+A1-A16
+B1-B16
+C1-C16
+D1-D16
+E1-E16
+F1-F16
+G1-G16
+H1-H16
+I1-I16
+J1-J16
+K1-J16
+L1-L16
+M1-M16
+N1-N16
+O1-O16
+P1-P16
+'''
+
+# Giant village - 676 acres
+
+'''
+A1-A26
+B1-B26
+C1-C26
+D1-D26
+E1-E26
+F1-F26
+G1-G26
+H1-H26
+I1-I26
+J1-J26
+K1-K26
+L1-L26
+M1-M26
+N1-N26
+O1-O26
+P1-P26
+Q1-Q26
+R1-R26
+S1-S26
+T1-T26
+U1-U26
+V1-V26
+W1-W26
+X1-X26
+Y1-Y26
+Z1-Z26
+'''
+
+# End of script
+
+"""
+File info:
+File type: Python script (*.py)
+File version: 1 (Wednesday, October 28th 2020 at 3:24 pm)
+Line count (including blank lines and compiler line): 88
+"""
