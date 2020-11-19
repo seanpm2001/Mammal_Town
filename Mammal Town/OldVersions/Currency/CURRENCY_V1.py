b8e5692a2cd8af82d38152dfad9a392d483e9edc
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-18 16:26:06 -0800

    CURRENCY_V1.py
    An archive of the currency listing for mammal town.

---
+++ Mammal Town/OldVersions/Currency/CURRENCY_V1.py
@@ -0,0 +1,33 @@
+# Mammal town in-game currency
+print ("This file is not meant to be opened by the game.")
+noMore = input("Press [ENTER] key to exit")
+''' Divider '''
+# The main currency is "balls" as it is similar to bells, but not the same. It can be used in some comedic ways too.
+player1BallCount = int(0)
+player1StarterBallCount = int(1000)
+# Different variations exist
+# Christmas/Holidays theme
+jingleBallCount = int(25)
+player1JingleBallCount = int(0)
+# Halloween theme
+pumpkinBallCount = int(31)
+player1PumpkinBallCount = int(0)
+# Valentines theme
+loveBallCount = int(14)
+player1LoveBallCount = int(0)
+# St. Pattys/Patricks day theme
+goldBallCount = int(21)
+player1GoldBallCount = int(0)
+''' Divider '''
+# Functions
+def newGame():
+  player1BallCount = player1BallCount + player1StarterBallCount
+def singleSell():
+  singleSell = int(0)
+  # This function is incomplete
+def sellApples():
+  sellAll.apples()
+  # This function is incomplete
+''' Divider '''
+# Being considered:
+# Soccer balls, basket balls, meat balls, fire balls, footballs, snow balls, beach balls, sea balls, etc.
