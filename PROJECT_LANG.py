ddb5fdf93ccb3823d22848a731fc72893eac41dc
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-11 18:23:19 -0800

    PROJECT_LANG.py
    The project language file for this project.

---
+++ PROJECT_LANG.py
@@ -0,0 +1,9 @@
+# Start of script
+# I have decided to write this game in Python, as it is my go to language and is highly functional and meets all the needs for this project.
+''' '''
+print ("I have decided to write this game in Python, as it is my go to language and is highly functional and meets all the needs for this project.")
+""" """
+# File version: 1 (Friday, December 11th 2020 at 6:23 pm)
+# File type: Python script file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
