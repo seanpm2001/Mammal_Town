85f6e0c1433bf48d54c46fae003a624f987c69e3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-11 18:20:37 -0800

    MT_TileEngineMainV1.py
    An archive of the first version of the tiling engine main script for Mammal town.

---
+++ Mammal Town/TilingEngine/OldVersions/MT_TileEngineMainV1.py
@@ -0,0 +1,44 @@
+# Start of script
+"""
+# Tiling engine for Mammal town
+
+Defines how items can be placed
+"""
+''' Definitions '''
+def itemsPerTile(): # The game has a grid of 1 item per tile
+  print ("1 item can be placed per tile")
+def rotateTile(): # Positions of how an object on a tile can be rotated
+  print ("Rotation types: 4/8")
+def drop(): # Dropping items
+  print ("Dropping item...")
+def place(): # Placing items
+  print ("Placing item...")
+def plant(): # Planting items
+  print ("Planting item...")
+def pickUp(): # picking up items
+  print ("Picking up item...")
+def dig(): # picking up items
+  print ("Digging...")
+def chop(): # chopping up items
+  print ("Chopping...")
+def chip(): # chipping up items
+  print ("Chipping...")
+def shake(): # shaking items
+  print ("Shaking...")
+def fishing(): # fishing for items
+  print ("Fishing...")
+def walkingFootprints(): # footprints from walking
+  print ("Footprints...")
+def runningFootprints(): # footprints from running
+  print ("Footprints...")
+"""
+''' Open file dialog '''
+print ("Tiling engine for Mammal town")
+noMore = input("This file isn't meant to be opened. Press [ENTER] key to quit")
+print ("The file should now be closed. If the file isn't closed, press the close button. If this doesn't work, end the task/process with a task manager/process manager")
+File info
+File type: Python script file (*.py)
+File version: 1 (Friday, December 11th 2020 at 6:20 pm)
+Line count (including blank lines and compiler line): 45
+"""
+# End of script
